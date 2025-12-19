868e26188630b8e4eae980b1e46e0746889afc95
Praveen-2122 <praveen2122.v@gmail.com>
2025-12-19 15:20:37 +0530

    updated

--- JS/Assignments/Day 17/Task 10.js
+++ JS/Assignments/Day 17/Task 10.js
@@ -3,6 +3,21 @@
 
 
 let score = 56;
+// let score = 56;
+
+// if (score >= 90){
+//     console.log("A");    
+// } else if( score >= 80){
+//     console.log("B");
+// }else if( score >= 70){
+//     console.log("C");
+// } else if ( score >= 50){
+//     console.log("D");    
+// } 
+// else{
+//     console.log("F");
+// }
+
 function getGrade(score) {
     if (score >= 90) {
         return "A";
