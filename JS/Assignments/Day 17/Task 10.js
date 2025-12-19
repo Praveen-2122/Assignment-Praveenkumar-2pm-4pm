4f4df59898403b6e3ab07ef017925acc1068b7bf
Praveen-2122 <praveen2122.v@gmail.com>
2025-12-19 15:24:21 +0530

    Updated

--- JS/Assignments/Day 17/Task 10.js
+++ JS/Assignments/Day 17/Task 10.js
@@ -1,23 +1,6 @@
 // Student Grading System
 // Use conditions to return A/B/C/D/F based on marks.
 
-
-let score = 56;
-// let score = 56;
-
-// if (score >= 90){
-//     console.log("A");    
-// } else if( score >= 80){
-//     console.log("B");
-// }else if( score >= 70){
-//     console.log("C");
-// } else if ( score >= 50){
-//     console.log("D");    
-// } 
-// else{
-//     console.log("F");
-// }
-
 function getGrade(score) {
     if (score >= 90) {
         return "A";
@@ -34,15 +17,4 @@ function getGrade(score) {
 }
 console.log(getGrade(85));
 
-if (score >= 90){
-    console.log("A");    
-} else if( score >= 80){
-    console.log("B");
-}else if( score >= 70){
-    console.log("C");
-} else if ( score >= 50){
-    console.log("D");    
-} 
-else{
-    console.log("F");
-}
+
