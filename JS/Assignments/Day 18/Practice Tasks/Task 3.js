77bc49e8a671ca6c756a550c3118d9c27021f6b8
Praveen-2122 <praveen2122.v@gmail.com>
2025-12-30 15:16:30 +0530

    Day 18 Done

---
+++ JS/Assignments/Day 18/Practice Tasks/Task 3.js
@@ -0,0 +1,6 @@
+// Create a for loop to print 1–10.
+
+for (let i = 1; i <= 10; i++){
+    console.log(i);
+    
+}
---
+++ JS/Assignments/Day 18/Practice Tasks/Task 4.js
@@ -0,0 +1,11 @@
+// Use while to print even numbers up to 20.
+
+
+let num = 0;
+
+while (num <= 20) {
+    if (num % 2 === 0) {
+        console.log(num);
+    }
+    num++;
+}
